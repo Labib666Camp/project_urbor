5f822affae7788c0150ac89970404b859504221b
Mainul Islam Labib <68593390+Labib666Camp@users.noreply.github.com>
2024-09-07 09:46:18 +0600

    Add files via upload

--- ee_authenticate.py
+++ ee_authenticate.py
@@ -1,5 +1,6 @@
 import ee
 import time as t
+import json
 
 def authenticate():
     st = t.time()
@@ -12,8 +13,8 @@ def authenticate():
     #     print(f"Error: {str(e)}")
     #     ee.Authenticate(auth_mode='localhost')
     #     ee.Initialize(project = 'ee-workmainulislam2')
-    service_account = 'urbor-serv-acc@project-urbor.iam.gserviceaccount.com'
-    credentials = ee.ServiceAccountCredentials(service_account, 'project-urbor-ac472ba741f1.json')
+    service_account = 'servaccforprojurborviz@project-urbor-visualization.iam.gserviceaccount.com'
+    credentials = ee.ServiceAccountCredentials(service_account, 'project-urbor-visualization-d57a1b2e6b05.json')
     ee.Initialize(credentials)
     print('-----DONE------')
     print(f'time taken ........ {t.time() - st}')
